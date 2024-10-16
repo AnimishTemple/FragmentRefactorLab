bb2c598bf32fa8c5664ba53d1e2b14ed433dbdc6
AnimishTemple <animishtenneti@gmail.com>
2024-10-16 15:28:58 -0400

    recreated fragment object as weird path glitch occurred

--- app/src/main/java/edu/temple/fragmentrefactor/nameFragment.kt
+++ app/src/main/java/edu/temple/fragmentrefactor/nameFragment.kt
@@ -6,19 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class NameEditFragment : Fragment() {
+
+class nameFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_name_edit, container, false)
+        return inflater.inflate(R.layout.fragment_name, container, false)
     }
-/*
-    companion object {
+
+    /*companion object {
         fun newInstance(param1: String, param2: String) =
-            NameEditFragment().apply {
+            nameFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
